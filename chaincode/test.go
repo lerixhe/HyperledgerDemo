cb328d45120c578268242922414a00a8d368f4d9
lerix <185734549@qq.com>
2019-08-14 05:01:01 +0800

    完成实现chaincode接口

--- chaincode/test.go
+++ chaincode/test.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 	_ "github.com/hyperledger/fabric/core/chaincode/shim"
-	_ "github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric/protos/peer"
 )
 
-func main() {
+// chaincode类型定义
+type Test struct {
+	// 简单的读写实现
+}
 
+func main() {
+	// 主函数中启动chaincode
+	shim.Start(new(Test))
+}
+
+// Test需要实现Chaincode接口,必须实现这两个方法：init invoke
+func (test *Test) Init(shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+func (test *Test) Invoke(shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
 }
