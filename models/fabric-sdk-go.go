f3e8b9a06dca33adb9a70627ceb633ac7791343d
lerix <185734549@qq.com>
2019-08-14 20:10:24 +0800

    新增依赖，完善sdk文件

--- models/fabric-sdk-go.go
+++ models/fabric-sdk-go.go
@@ -7,17 +7,19 @@
 package models
 
 import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
 )
 
 
 const (
-    channelID      = "default"
-    orgName        = "bcs06071508"
+    channelID      = "itcastchannel"
+    orgName        = "OrgGoMSP"
     orgAdmin       = "Admin"
-    ordererOrgName = "bcs06071508"
-    ccID           = "obcs-example02"
+    ordererOrgName = "OrdererOrg"
+    ccID           = "testcc"
 )
 /* 获取client对象  */
 func getClient(configOpt core.ConfigProvider, sdkOpts ...fabsdk.Option)*channel.Client{
@@ -34,7 +36,7 @@ func getClient(configOpt core.ConfigProvider, sdkOpts ...fabsdk.Option)*channel.
 			  client, err := channel.New(clientChannelContext)
 
 			  if err != nil {
-				  fmt.Println("Failed to create new channel client: %s", err)
+				  logs.Info("Failed to create new channel client: %s", err)
 			  }
 			  return client
 }
@@ -46,7 +48,14 @@ type ChainCodeSpec struct {
 	client      channel.Client
 	chaincodeId string
 }
-
+/* 获取 操作智能合约的对象*/
+func NewChainCodeSpec()*ChainCodeSpec{
+	configPath := beego.AppConfig.String("CORE_Testcc_CONFIG_FILE")
+	return &ChainCodeSpec{
+		client:getClient(config.FromFile(configPath)),
+		chaincodeId:channelID
+	},nil
+}
 /* 查询 */
 func (this *ChainCodeSpec) ChaincodeQuery(function string, chaincodeArgs [][]byte) (response []byte, err error) {
 	res :=
